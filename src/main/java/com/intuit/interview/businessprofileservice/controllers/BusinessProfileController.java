016e24f43dc2132204e85e5bc3b978db7b983a7d
DevilsZone <akash.grayfrost.app>
2023-09-06 09:36:48 +0530

    Handling CORS

--- src/main/java/com/intuit/interview/businessprofileservice/controllers/BusinessProfileController.java
+++ src/main/java/com/intuit/interview/businessprofileservice/controllers/BusinessProfileController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/business-profile")
+@CrossOrigin("http://localhost:3000")
 public class BusinessProfileController {
     private final BusinessProfileService businessProfileService;
 
